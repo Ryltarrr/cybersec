410d8e3961198938cb8e0d5586f3461a2bbd46bb
Justin Guerré <guerrejustin@gmail.com>
2021-09-22 11:10:29 +0200

    added ProcessController

---
+++ src/Controller/ProcessController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Process;
+use App\Repository\IngredientRepository;
+use App\Repository\ProcessRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ProcessController extends AbstractController
+{
+    /**
+     * @Route("/process", name="process", methods={"GET"})
+     */
+    public function index(ProcessRepository $processRepo): JsonResponse
+    {
+        $processes = $processRepo->findAll();
+        return $this->json($processes);
+    }
+
+    /**
+     * @Route("/process", name="create_process", methods={"POST"})
+     */
+    public function createProcess(
+        Request $request,
+        ValidatorInterface $validator,
+        EntityManagerInterface $entityManager
+    ) {
+        $body = $request->toArray();
+
+        $name = $body["name"];
+        $description = $body["description"];
+        $tests = $body["tests"];
+
+        $process = new Process();
+        $process->setName($name);
+        $process->setDescription($description);
+        $process->setTests($tests);
+
+        $errors = $validator->validate($process);
+        if (count($errors) > 0) {
+            return $this->json($errors, 400);
+        }
+
+        $entityManager->persist($process);
+        $entityManager->flush();
+
+        return $this->json($process->getId());
+    }
+
+    /**
+     * @Route("/process", name="update_process", methods={"PUT"})
+     */
+    public function updateProcess(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        ProcessRepository $processRepo
+    ): JsonResponse {
+        $body = $request->toArray();
+        $id = $body["id"];
+
+        $process = $processRepo->find($id);
+
+        if (!$process) {
+            $this->createNotFoundException("Wrong id");
+        }
+
+        $name = $body["name"];
+        $description = $body["description"];
+        $tests = $body["tests"];
+
+        if ($name) {
+            $process->setName($name);
+        }
+
+        if ($description) {
+            $process->setDescription($description);
+        }
+
+        if ($tests) {
+            $process->setTests($tests);
+        }
+
+        $entityManager->persist($process);
+        $entityManager->flush();
+
+        return $this->json($process->getId());
+    }
+
+    /**
+     * @Route("/process/{id}", name="process_process", methods={"DELETE"})
+     */
+    public function deleteIngredient(
+        int $id,
+        ProcessRepository $processRepo,
+        EntityManagerInterface $entityManager
+    ) {
+        $process = $processRepo->find($id);
+
+        if (!$process) {
+            $this->createNotFoundException("Wrong id");
+        }
+
+        $entityManager->remove($process);
+        $entityManager->flush();
+
+        return $this->json($id);
+    }
+}
